aa1df5179f467532ffb49c5192c40442c0e93d66
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-08-16 11:17:45 -0300

    Feat: Adding encryption to passwords

--- server/src/controller/UserController.ts
+++ server/src/controller/UserController.ts
@@ -1,7 +1,8 @@
+import cors from "cors";
 import express from "express";
 import { Router } from "express";
+import * as bcrypt from "bcryptjs";
 import { PrismaClient } from "@prisma/client";
-import cors from "cors";
 
 const prisma = new PrismaClient();
 const router = Router();
@@ -33,15 +34,25 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ error: "Email já está em uso" });
     }
 
-    // Criar o novo usuário
-    const newUser = await prisma.user.create({
-      data: {
-        email,
-        password,
-      },
+    // Hash da senha
+    bcrypt.genSalt(10, async (err, salt) => {
+      if (err) {
+        throw err;
+      }
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) {
+          throw err;
+        }
+        // Criar o novo usuário com a senha criptografada
+        const newUser = await prisma.user.create({
+          data: {
+            email,
+            password: hash, // Use o hash da senha
+          },
+        });
+        res.status(201).json({ message: "Usuário registrado com sucesso" });
+      });
     });
-
-    res.status(201).json({ message: "Usuário registrado com sucesso" });
   } catch (error) {
     console.error("Erro ao registrar usuário:", error);
     res.status(500).json({ error: "Erro ao registrar usuário" });
